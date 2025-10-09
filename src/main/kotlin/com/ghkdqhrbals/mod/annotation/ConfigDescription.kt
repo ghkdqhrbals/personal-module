60c56997a34438e7f7831faca52788aa1af3c7f9
ghkdqhrbals <ghkdqhrbals@naver.com>
2025-10-09 17:56:15 +0900

    fix: descriptions addd

--- src/main/kotlin/com/ghkdqhrbals/mod/annotation/ConfigDescription.kt
+++
@@ -1,4 +0,0 @@
-package com.ghkdqhrbals.mod.annotation
-
-@Target(AnnotationTarget.PROPERTY)
-annotation class ConfigDescription(val value: String)
--- src/main/kotlin/com/ghkdqhrbals/mod/config/OauthProvidersProperties.kt
+++ src/main/kotlin/com/ghkdqhrbals/mod/config/OauthProvidersProperties.kt
@@ -1,23 +1,15 @@
 package com.ghkdqhrbals.mod.config
 
-import com.ghkdqhrbals.mod.annotation.ConfigDescription
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 
 data class OauthProviderProperties(
-    @ConfigDescription("OAuth Client ID")
     var clientId: String = "",
-    @ConfigDescription("OAuth Client Secret")
     var clientSecret: String = "",
-    @ConfigDescription("OAuth Redirect URI")
     var redirectUri: String = "",
-    @ConfigDescription("OAuth Token Path")
     var tokenPath: String? = "",
-    @ConfigDescription("OAuth Code Path")
     var codePath: String? = "",
-    @ConfigDescription("OAuth User Info Path")
     var userInfoPath: String? = "",
-    @ConfigDescription("OAuth Scopes")
     var scopes: List<String> = emptyList(),
 )
 
