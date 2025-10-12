a3ddf00c72df4ce88f6d0c2c55b05e7507cee423
ghkdqhrbals <ghkdqhrbals@naver.com>
2025-10-12 17:57:55 +0900

    feat: initialize OAuth and Time applications with basic structure

---
+++ time/src/test/kotlin/org/ghkdqhrbals/time/TimeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ghkdqhrbals.time
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
