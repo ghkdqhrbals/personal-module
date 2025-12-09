dff9938be0ab2dc45c52f748554abd4e93ae20dd
ghkdqhrbals <ghkdqhrbals@naver.com>
2025-12-10 01:58:11 +0900

    feat: restructure repository to infra, update package paths, and add initial schema and data configuration

---
+++ core/schema/src/main/kotlin/org/ghkdqhrbals/schema/SchemaApplication.kt
@@ -0,0 +1,11 @@
+package org.ghkdqhrbals.schema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchemaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchemaApplication>(*args)
+}
---
+++ core/schema/src/test/kotlin/org/ghkdqhrbals/schema/SchemaApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ghkdqhrbals.schema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchemaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
