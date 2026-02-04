ff2bbefb1c8147597becb65654c9d8166ef11180
ghkdqhrbals <ghkdqhrbals@naver.com>
2026-02-05 02:03:58 +0900

    feat: disable allow_credentials in CORS settings in app.py

--- etc/guestbook/app.py
+++ etc/guestbook/app.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:4000",
         "https://ghkdqhrbals.github.io",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
